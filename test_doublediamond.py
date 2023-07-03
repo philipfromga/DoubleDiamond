d082ab6091fea1a4c8e2561914e7a95e51105454
Philip Rodgers <philiprodgers@Philips-M2-MacBook-Pro.local>
2023-07-03 16:46:01 -0600

    working on test_harness

--- test_doublediamond.py
+++ test_doublediamond.py
@@ -1,7 +1,12 @@
 import DoubleDiamond
-from DoubleDiamond import Balance, spinwheel
+from DoubleDiamond import Balance, spinwheel, play_or_quit
+from io import StringIO
 
-def test_default_initial_balance():
+
+def test_default_initial_balance(monkeypatch):
+    mock_input = StringIO("yes\n")
+    monkeypatch.setattr('sys.stdin', mock_input)
+    monkeypatch.setattr("DoubleDiamond.play_or_quit", lambda x: "yes")
     test_balance = Balance(47)
     assert test_balance.total == 47
     assert spinwheel() in {"-","=","≡","7","%","◇"}
